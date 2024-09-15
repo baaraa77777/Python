0562193337de075dc3ea6d6893563882d08e8d86
Radhika <basnetrd1@gmail.com>
2024-09-15 19:28:37 +0545

    variables

--- stringmanipulation.py
+++ stringmanipulation.py
@@ -1,4 +1,2 @@
 print("Hello World!! \n Hello World!!")
 
-#string concatenation
-print("Radhika " + "Basnet")
---
+++ variables.py
@@ -0,0 +1,12 @@
+#WAP to swap the values entered.
+
+a=input("a: ")
+b=input("b: ")
+
+c=a
+a=b
+b=c
+
+print('After swapping: ')
+print('a: '+ str(a))
+print('b: '+ str(b))
