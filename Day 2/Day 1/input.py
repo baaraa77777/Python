d8995981e0d6e97d9db16b9de36aa55f195de93d
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-09-17 17:27:15 +0545

    Delete Day 2/Day 1 directory

--- Day 2/Day 1/input.py
+++
@@ -1,3 +0,0 @@
-#input()will get user input in output.
-#input("What is your name?")
-print("Hello "+ input("What is your name?") + "!")
--- Day 2/Day 1/stringconcat.py
+++
@@ -1,2 +0,0 @@
-#string concatenation
-print("Radhika" +" "+ "Basnet") 
