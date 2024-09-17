5d43d8d0539b6ab49737f39774496d7ba2d1be08
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-09-17 17:27:48 +0545

    Delete Day 1/Day 1 directory

--- Day 1/Day 1/input.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD:input.py
-#input exercise
-size=len(input('Enter your name.'))
-print("The number of characters in the entered name is "+ str(size))
-=======
-#input()will get user input in output.
-#input("What is your name?")
-print("Hello "+ input("What is your name?") + "!")
->>>>>>> 6ca9798ff97817c6adb37e42ec027d4cdee54802:Day 1/input.py
