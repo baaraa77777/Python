a3d3be435b3d1e26f9db60bc4e07cfaa6e95be9c
Radhika <basnetrd1@gmail.com>
2024-09-17 18:05:48 +0545

    nested

--- day 3/nestedif.py
+++ day 3/nestedif.py
@@ -5,7 +5,9 @@ if height >=120:
     print("You can ride the rollercoaster. ")
 
     age=int(input("Enter your age: "))
-    if age<=18:
+    if age<12:
+        print("You have to pay $5. ")
+    elif age<=18:
         print("You have to pay $7. ")
     else:
         print("You have to pay $12. ")
