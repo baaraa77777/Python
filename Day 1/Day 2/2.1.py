52155fb236bdf26c51e4f4aa1106241ecdf7f577
Radhika Basnet <162020845+baaraa77777@users.noreply.github.com>
2024-09-17 17:28:02 +0545

    Delete Day 1/Day 2 directory

--- Day 1/Day 2/2.1.py
+++
@@ -1,9 +0,0 @@
-#program to add digits of a number.
-
-two_digit_num=(input("Enter a two-digit number: "))
-
-sum=int(two_digit_num[0])+int(two_digit_num[1])
-
-print ('The sum of the digits of a number is '+ str(sum))
-
-print(type(sum))
