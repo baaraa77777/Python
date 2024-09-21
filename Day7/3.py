48d6f99c5dcafc8d99392f75fe36e83e4ee23510
Radhika <basnetrd1@gmail.com>
2024-09-21 21:30:56 +0545

    Checking the end of game

---
+++ Day7/3.py
@@ -0,0 +1,29 @@
+import random
+word_list = ["mouse", "apples", "baboon"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
