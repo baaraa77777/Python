d6be27b717e8bdffba383976422aeaa9357ef343
Radhika <basnetrd1@gmail.com>
2024-09-21 22:41:21 +0545

    Final result of hangman game.

---
+++ Day7/4.py
@@ -0,0 +1,42 @@
+import random
+
+import hangman_words
+chosen_word = random.choice(hangman_words.word_list)
+
+import hangman_art
+print(hangman_art.logo)
+lives =  6
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}.")
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -=1
+        if lives ==0:
+                end_of_game = True
+                print('You lose.')
+                print(f"The word is {chosen_word}.")
+    print(f"{' '.join(display)}")
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+       
+    print(hangman_art.stages[lives])
---
+++ Day7/hangman_words.py
@@ -0,0 +1,19 @@
+word_list= ['electricity', 'donkey', 'hardware', 'xerox', 'transistor', 'computer', 'desktop'
+ 'engineering', 'hangman', 'circuit', 'imagination', 'robot', 'memory', 'power', 
+ 'submarine', 'chess', 'resistance', 'matrix', 'function', 'laser', 'mechanism', 
+ 'bodyguard', 'titanic', 'global', 'ozone', 'bridge', 'technology', 'spider', 
+ 'pyramid', 'sphere', 'member', 'warning', 'yourself', 'screen', 'language', 
+ 'system', 'internet', 'parameter', 'traffic', 'network', 'filter', 'nucleus', 
+ 'automatic', 'microphone', 'cassette', 'operation', 'country', 'beautiful', 
+ 'picture', 'teacher', 'superman', 'undertaker', 'alarm', 'process', 'keyboard', 
+ 'electron', 'certificate', 'grandfather', 'landmark', 'relativity', 'eraser', 
+ 'design', 'football', 'human', 'musician', 'egyptian', 'elephant', 'queen', 
+ 'message', 'wallpaper', 'nationality', 'answer', 'wrong', 'statement', 'forest',
+ 'puzzle', 'voltage', 'current', 'mathematics', 'wisdom', 'dream', 'supermarket',
+ 'database', 'collection', 'barrier', 'project', 'sunlight', 'figure', 'graph', 
+ 'battle', 'hundred', 'signal', 'thousand', 'transformation', 'daughter', 'flower'
+ 'communication', 'microwave', 'electronic', 'peace', 'wireless', 'delete', 'wind'
+ 'brain', 'control', 'prophet', 'freedom', 'harbour', 'confidence', 'positive', 
+ 'harvest', 'hunger', 'woman', 'children', 'stranger', 'garden', 'pleasure', 
+ 'between', 'recognition', 'tomorrow', 'autumn', 'monkey', 'spring', 'winter', 
+ 'classification', 'typewriter']
