d293eda2732527da5ab3c0d7f0a0ea6254d09fc4
Radhika <basnetrd1@gmail.com>
2024-09-25 11:26:36 +0545

    Funtion with multiple returns

---
+++ Day 10/finction().py
@@ -0,0 +1,11 @@
+def funct(f_name, l_name):
+    f =f_name.title()
+    l =l_name.title()
+    return f + l
+
+first_name = "RadhiKa "
+last_name = "BAsnet"
+
+name = funct(first_name, last_name)
+
+print(f"My name is {name}.")
